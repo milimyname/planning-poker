7c4a6c530a49a9d5449a78c69959290c308d8b62
mili <kamil.maksudov2019@gmail.com>
2024-10-30 20:19:39 +0100

    fix: run local base

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,8 @@
 import { browser } from '$app/environment';
 
-export const BASE_URL = 'https://electric-on-fly-test-app-broken-tree-4950.fly.dev';
+// export const BASE_URL = 'https://electric-on-fly-test-app-broken-tree-4950.fly.dev';
 
-// export const BASE_URL = import.meta.env.ELECTRIC_URL ?? `http://localhost:3000`;
+export const BASE_URL = import.meta.env.ELECTRIC_URL ?? `http://localhost:3000`;
 
 // Caddy server
 // export const BASE_URL = import.meta.env.ELECTRIC_URL ?? `http://localhost:3002`;
