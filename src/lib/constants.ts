44a1aea1f4575784ae679e040deae7853a0189cb
Komiljon Maksudov <kamil.maksudov2019@gmail.com>
2025-03-12 22:24:59 +0100

    chore: add public env

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,15 +1,13 @@
 import { browser } from '$app/environment';
-import { PUBLIC_ELECTRIC_URL } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 
 // Caddy server
-// export const BASE_URL = PUBLIC_ELECTRIC_URL ?? `http://localhost:3001`;
+// export const BASE_URL = env.PUBLIC_ELECTRIC_URL ?? `http://localhost:3001`;
 
-export const BASE_URL = PUBLIC_ELECTRIC_URL ?? `http://localhost:3000`;
-
-console.log('BASE_URL', BASE_URL);
+export const BASE_URL = env.PUBLIC_ELECTRIC_URL ?? `http://localhost:3000`;
 
 export const BASE_API_URL = browser
 	? `${window.location.origin}/api/shapes`
-	: 'http://localhost:5173/api/shapes'; // fallback for server-side rendering
+	: 'http://localhost:5173/api/shapes';
 
 export const ESTIMATE_SELECTIONS = ['1,2,3,5,8,13,21'];
